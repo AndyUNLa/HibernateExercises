badc00f874fbfbb4a5cc2fcb6a8bdf193ab3fe81
Andy-thor <andy.dev536@gmail.com>
2020-04-02 19:59:23 -0300

    Added hibernate maven project

---
+++ hibernate-mvn/src2/negocio/ClienteABM.java
@@ -0,0 +1,50 @@
+package negocio;
+import java.time.LocalDate;
+import java.util.List;
+import dao.ClienteDao;
+import datos.Cliente;
+
+public class ClienteABM {
+	ClienteDao dao = new ClienteDao();
+
+	public Cliente traer(long idCliente) {
+		Cliente c = dao.traer(idCliente);
+		return c;
+	}
+
+	public Cliente traer(int dni) {
+		Cliente c = dao.traer(dni);
+		return c;
+	}
+
+	public int agregar(String apellido, String nombre, int dni, LocalDate fechaDeNacimiento) throws Exception {
+		if(traer(dni) != null) {
+			throw new Exception("No se puede agregar el Cliente. El dni ya existe");
+		}
+		Cliente c = new Cliente(apellido, nombre, dni, fechaDeNacimiento);
+		return dao.agregar(c);
+	}
+	
+	public void modificar(Cliente c) throws Exception {
+		if(traer(c.getDni()) == null) {
+			throw new Exception("No se puede modificar el Cliente. El dni no existe");
+		} else if(traer(c.getIdCliente()) == null) {
+			throw new Exception("No se puede modificar el Cliente. El id no existe");
+		}
+		dao.actualizar(c);
+	}
+	
+	public void eliminar(long idCliente) throws Exception {
+		//en este caso es física en gral. no se aplicaría este caso de uso, si
+		//se hiciera habría que validar que el cliente no tenga dependencias
+		Cliente c = dao.traer(idCliente);
+		if(c == null) {
+			throw new Exception("No se pudo realizar la operación. El cliente no existe");
+		}
+		dao.eliminar(c);
+	}
+
+	public List<Cliente> traer() {
+		return dao.traer();
+	}
+}
