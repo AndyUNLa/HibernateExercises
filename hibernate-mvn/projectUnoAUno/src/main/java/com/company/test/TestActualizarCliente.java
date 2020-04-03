c63a3e3a723fac8c169dd01effea29c2ae09b370
Andy-thor <andy.dev536@gmail.com>
2020-04-03 16:45:40 -0300

    Created projectUnoAUno

---
+++ hibernate-mvn/projectUnoAUno/src/main/java/com/company/test/TestActualizarCliente.java
@@ -0,0 +1,25 @@
+package com.company.test;
+import com.company.datos.Cliente;
+import com.company.negocio.ClienteABM;
+
+public class TestActualizarCliente {
+	public static void main(String[] args) {
+		ClienteABM abm = new ClienteABM();
+		long id = 1;
+		// traer el obj a modificar
+		Cliente c = abm.traer(id);
+		System.out.println("Cliente a Modificar -->" + c);
+		// modificar por set los atributos
+		c.setDni(35000001);
+		try {
+			abm.modificar(c);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		 // update del objeto
+		int dni = 35000001;
+		Cliente cModif = abm.traer(dni);
+		System.out.println("Cliente Modificado -->" + cModif);
+	}
+}
