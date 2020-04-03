cded921eb2a8d6241b964174c70c49c7046e29cb
Andy-thor <andy.dev536@gmail.com>
2020-04-03 15:14:56 -0300

    Changed previous removed folder to this

---
+++ hibernate-mvn/projectUnaEntidad/src/main/java/com/company/test/TestHBM.java
@@ -0,0 +1,12 @@
+package com.company.test;
+import org.hibernate.Session;
+import com.company.dao.HibernateUtil;
+
+public class TestHBM {
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.close();
+		System.out.println("OK");
+	}
+}
