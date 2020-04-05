f211836b4bc16926c9e17557500fb369a51fea5a
Andy-thor <andy.dev536@gmail.com>
2020-04-05 13:59:57 -0300

    Renamed old folder

---
+++ exercises+mvn/hibernate-una-entidad/src/main/java/com/company/test/TestAgregarCliente.java
@@ -0,0 +1,22 @@
+package com.company.test;
+import java.time.LocalDate;
+import java.util.function.LongToIntFunction;
+
+import com.company.negocio.ClienteABM;
+
+public class TestAgregarCliente {
+	public static void main(String[] args) {
+		String apellido = "Perez";
+		String nombre = "Juan";
+		int dni = 32000001;
+		// tu fecha de nacimiento
+		LocalDate fechaDeNacimiento = LocalDate.now();
+		ClienteABM abm = new ClienteABM();
+		try {
+			long ultimoIdCliente = abm.agregar(apellido, nombre, dni, fechaDeNacimiento);
+			System.out.println("Agregado satisfactoriamente. ID: " + Long.toString(ultimoIdCliente));
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
