a7d904a3220e53c349cbf81a190d3c65e20f458f
andyunla <andy.dev.alnu@hotmail.com>
2020-04-08 23:45:07 -0300

    Update comments for tests

--- exercises+mvn/hibernate-uno-a-uno/src/main/java/com/company/test/TestAgregarCliente.java
+++ exercises+mvn/hibernate-uno-a-uno/src/main/java/com/company/test/TestAgregarCliente.java
@@ -2,19 +2,26 @@ package com.company.test;
 import java.time.LocalDate;
 
 import com.company.negocio.ClienteABM;
+import com.company.funciones.LoggerWrapper;
 
+/*
+ * Test para agregar un nuevo cliente
+ */
 public class TestAgregarCliente {
 	public static void main(String[] args) {
-		String apellido = "Perez";
-		String nombre = "Juan";
-		int dni = 32000001;
+		String className = TestAgregarCliente.class.getName();
+        LoggerWrapper logger = LoggerWrapper.getInstance(className, true);
+		String apellido = "Downey Jr";
+		String nombre = "Robert";
+		int dni = 34000001;
 		LocalDate fechaDeNacimiento = LocalDate.now();
 		ClienteABM abm = new ClienteABM();
+		logger.info("Agregar un nuevo cliente \n\n");
 		try {
-			long ultimoIdCliente = abm.agregar(apellido, nombre, dni, fechaDeNacimiento, null);
-			System.out.println("Agregado satisfactoriamente. ID: " + Long.toString(ultimoIdCliente));
+			long ultimoIdCliente = abm.agregar(apellido, nombre, dni, fechaDeNacimiento);
+			logger.info("Agregado satisfactoriamente. ID: " + Long.toString(ultimoIdCliente));
 		} catch(Exception e) {
-			System.out.println(e.getMessage());
+			logger.error(e.getMessage());
 		}
 	}
 }
--- exercises+mvn/hibernate-uno-a-uno/src/main/java/com/company/test/TestTraerClienteYContacto.java
+++ exercises+mvn/hibernate-uno-a-uno/src/main/java/com/company/test/TestTraerClienteYContacto.java
@@ -1,17 +1,25 @@
 package com.company.test;
 
 import com.company.datos.Cliente;
+import com.company.funciones.LoggerWrapper;
 import com.company.negocio.ClienteABM;
 
+/*
+ * Test que obtiene un cliente y sus
+ * datos de contacto dado su ID
+*/
 public class TestTraerClienteYContacto {
     public static void main(String[] args) {
+        String className = TestTraerClienteYContacto.class.getName();
+        LoggerWrapper logger = LoggerWrapper.getInstance(className, true);
         ClienteABM abm = new ClienteABM();
         long idCliente = 1;
         try {
+            logger.info("Traer Cliente y contacto dado idCliente=" + idCliente);
             Cliente cliente = abm.traerClienteYContacto(idCliente);
-            System.out.println("\nTraer Cliente y contacto:\n" + cliente);
+            logger.info("Cliente y datos de contacto: \n" + cliente);
         } catch(Exception e) {
-            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
         }
     }
 }
