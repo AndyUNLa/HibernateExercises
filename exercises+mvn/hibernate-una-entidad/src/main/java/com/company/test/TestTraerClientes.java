91e302890579a1fd2f9d3b04f034f02fe714dd8d
andyunla <andy.dev.alnu@hotmail.com>
2020-04-08 23:48:52 -0300

    Update comments for tests

--- exercises+mvn/hibernate-una-entidad/src/main/java/com/company/test/TestTraerClientes.java
+++ exercises+mvn/hibernate-una-entidad/src/main/java/com/company/test/TestTraerClientes.java
@@ -1,13 +1,19 @@
 package com.company.test;
 import java.util.List;
-import java.util.ArrayList;
 import com.company.datos.Cliente;
+import com.company.funciones.LoggerWrapper;
 import com.company.negocio.ClienteABM;
 
+/*
+ * Test que trae todos clientes
+ */
 public class TestTraerClientes {
     public static void main(String[] args) {
+        String className = TestTraerClientes.class.getName();
+        LoggerWrapper logger = LoggerWrapper.getInstance(className, true);
         ClienteABM abm = new ClienteABM();
         List<Cliente> list = abm.traer();
+        logger.info("Traer todos los clientes \n\n");
         for (Cliente cliente : list) {
             System.out.println(cliente);
         }
